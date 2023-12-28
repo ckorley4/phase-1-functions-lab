0a5fbc895369bc99a567786aea5cc2118fc0cff4
ckorley4 <ckorley4uo@yahoo.com>
2023-12-28 12:44:41 -0500

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  return distance < 43 ? 8 : (distance % 43) + 1
+}
+
+function distanceFromHqInFeet(distance) {
+  return distance < 43 ? 8 * 264 : ((distance % 43) + 1) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = distanceTravelledInFeet(start, end)
+  console.log(distance)
+  return distance > 2500
+    ? 'cannot travel that far'
+    : distance >= 2000
+    ? 25
+    : distance > 400 && distance <= 2000
+    ? (distance - 400) * 0.02
+    : 0
+}
